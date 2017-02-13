6158b8276b52fd2890047cf1f7dd8bba0128ae57
Luis Leon <lleon95@vps291633.ovh.net>
2017-02-13 01:21:14 +0100

    Texto de bienvenida listo

--- Experimentos_LuisLeon/welcome.asm
+++ Experimentos_LuisLeon/welcome.asm
@@ -20,12 +20,32 @@ section .text
 	global _start ; Iniciará con _welcome
 
 _start:
-	; ### Parte 1 - Texto de Bienvenido ###
+	; ### Parte 1 - Headers o Gatos ###
+	mov rax,1						;Colocar en modo sys_write
+	mov rdi,1						;Colocar en consola
+	mov rsi,const_headers_txt				;Cargar los headers para imprimirlos
+	mov rdx,const_headers_size				;Tamaño de los headers
+	syscall							;Ejecutar
+	; ### Parte 2 - Texto de Bienvenido ###
 	mov rax,1 						; Colocar en modo sys_write
 	mov rdi,1 						; Colocar en consola
-	mov rsi,const_bienvenido_txt 	; Colocar el texto a imprimir -  Bienvenido
-	mov rdx,const_bienvenido_size	; Colocar el tamaño del texto - Bienvenido
+	mov rsi,const_bienvenido_txt			 	; Colocar el texto a imprimir -  Bienvenido
+	mov rdx,const_bienvenido_size				; Colocar el tamaño del texto - Bienvenido
 	syscall
+	; ### Parte 3 - Texto de Curso ###
+	mov rax,1						; Colocar en modo sys_write
+	mov rdi,1                                               ; Colocar en consola
+        mov rsi,const_curso_txt         	                ; Colocar el texto a imprimir - Curso
+        mov rdx,const_curso_size      	                        ; Colocar el tamaño del texto - Curso
+	syscall
+        ; ### Parte 4  - Headers o Gatos ###
+        mov rax,1                                               ;Colocar en modo sys_write
+        mov rdi,1                                               ;Colocar en consola
+        mov rsi,const_headers_txt                               ;Cargar los headers para imprimirlos
+        mov rdx,const_headers_size                              ;Tamaño de los headers
+        syscall  
+	; ### Exit ###
 	mov rax,60						; Salir del sistema sys_exit
 	mov rdi,0
 	syscall
+ 
