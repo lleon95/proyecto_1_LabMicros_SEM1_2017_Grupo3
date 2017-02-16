62ae5c3d84e337976f7c749e4cb468d809d85845
Luis Leon <lleon95@estudiantec.cr>
2017-02-15 21:54:02 -0600

    Completar el Fetch y agregar banco de registros

--- Experimentos_LuisLeon/PruebaFetcher.asm
+++ Experimentos_LuisLeon/PruebaFetcher.asm
@@ -18,21 +18,26 @@ section	.data
   ; ### Parte 3 - Comprobación de correcto ###
   fd dw 0
 
-  ; ### Parte X - Mensaje de error FILENOTFOUND ###
+  ; ### Parte B - Mensaje de error FILENOTFOUND ###
   const_filenotfound_txt: db 'Archivo ROM.txt no encontrado', 0xa
   const_filenotfound_size: equ $-const_filenotfound_txt
-  ; ### Parte X - Mensaje de info FileFound ###
+  ; ### Parte A - Mensaje de info FileFound ###
   const_filefound_txt: db 'Archivo ROM.txt encontrado', 0xa
   const_filefound_size: equ $-const_filefound_txt
-  
-  ; ### Parte Fetch ###
-  instructions TIMES 150 dw -1   ; Cargar de FF el arreglo de instrucciones
+  ; ### Parte C - Mensaje de error - Overflow de instrucciones ###
+  const_instoverflow_txt: db 'Error: Existen más instrucciones de las permitidas (150)', 0xa
+  const_instoverflow_size: equ $-const_instoverflow_txt
 
+  ; ### Parte Fetch ###
+  instructions TIMES 150 dw -1   ; Cargar el arreglo de instrucciones 150 inst
+  data TIMES 401 dw -1           ; Cargar el arreglo de memoria en 401 (0x190) words
+  stack TIMES 100 dw -1          ; Cargar el arreglo de stack de 100 palabras --- DUDA!!!!
+  registers TIMES 32 dw 0        ; Cargar los registros del microprocesador
 
 section	.text
-   global CMAIN         ;must be declared for using gcc
+   global _start         ;must be declared for using gcc
 
-CMAIN:
+_start:
     mov rbp, rsp; for correct debugging
   ; ### Parte 1 - Mensaje de buscando archivo ###
   mov rax,1						             ;Colocar en modo sys_write
@@ -49,64 +54,106 @@ CMAIN:
   syscall
 
   ; ### Parte 3 - Comprobación de correcto ###
-  mov [fd], rax                      ; Apertura del puntero 
+  mov [fd], rax                      ; Apertura del puntero
   mov	rdx,0
   cmp	rdx,rax                      ; Condicion si hay bytes
   jg	_filenotfound                ; Si hay una incongruencia
   jmp _filefound                     ; Mensaje de encontrado
 
-_startPCCounter:
-  mov r15, 0                  ; Inicializar en el PC Counter
-  mov r14, 150                       ; Total de instrucciones 
+  ; ### Parte 4 - Inicializar la carga de las instrucciones a memoria de instrucciones ###
+_readinstructions:
+  mov r15, 0                        ; Inicializar en el PC Counter
+  mov r14, 150                       ; Total de instrucciones
+  mov r12, instructions              ; Copiar el puntero de memoria a r12
 
 _fileread:
-  ; ### Parte 4 - Leer ###
+  ; ### Parte 5 - Leer una instruccion (32 bits) ###
   mov rax, SYS_READ
   mov rdi, [fd]
   mov rsi, file_buffer
   mov rdx, BUFFER_SIZE
   syscall
-  
-  ; Insertar en el arreglo
+
+  ; ### Parte 6 - Verificar overflow de instrucciones (más de 150) ###
   mov r13, r14                  ; Hace copia de los registros totales
-  sub r13, r15           ; Resta de los registros totales con el PC Counter
+  sub r13, r15                  ; Resta de los registros totales con el PC Counter
   cmp r13, 0                    ; Si la resta es menor, hay overflow
-  jl _exit
-  
-  ; Agregar instrucciones al arreglo
-  mov r13, [file_buffer]
-  mov [instructions], r13
-  add r15, 1              
-  
-  ; Ver si se terminó de leer
+  jl _instoverflow
+
+  ; ### Parte 7 - Agregar instrucciones al arreglo de instrucciones ###
+  mov r13, [file_buffer]        ; Copiar la instruccion en un registro temporal
+  mov [r12], r13                ; Añadir la instrucción al arreglo
+  add r15, 1                    ; Agregar 1 al PC
+  add r12, 1                    ; Mover el puntero del arreglo al siguiente elemento
+
+  ; ### Parte 7 - Validar fin de lectura ###
   cmp rax, 0
-  je _exit
+  je _prefetch
 
-  ; Mostrar contenido en consola
-  ;mov rdx, rax
-  ;mov rax, SYS_WRITE
-  ;mov rdi, STDOUT
-  ;mov rsi, file_buffer
-  ;syscall
-
+  ; ### Parte 8- Retorno a continuar leyendo otra instruccion ###
   jmp _fileread
-  
+
+_prefetch:
+  ; ### Parte 9- Preparar el PC y apuntarlo en la posicion inicial ###
+  mov r14, r15                ; Repaldar las instrucciones totales que existen (para evitar desbordamientos)
+  mov r15, 0x400000           ; Colocar el PC Counter en su posicion inicial
+
+; DEBUG! FALTA PROBAR
+_fetch:
+  ; ### Parte 10 - Decodificar el PC en memoria de instrucciones: Pasar de 400004 a 1 ###
+  mov r14, r15                ; Hacer copia del PC
+  shr r14, 1                  ; Dividir por 4
+  and r14, 0xFFF              ; Obtener los últimos dígitos
+  ; Verificar que el contador sea válido
+  cmp r14, 150                ; Ver si no hay overflow
+  jge _instoverflow           ; Si es 150 o más, hay overflow
+  ; Hasta este punto, ya tengo el valor del puntero del arreglo a incrementar
+
+  ; ### Parte 11 - Buscar la instrucción (Fetching) ###
+  add r14, instructions       ; Sumar elemento al arreglo respectivo
+  mov rdx, [r14]              ; Cargar la instruccion en rdx
+  ; Hasta este punto, tengo la próxima instrucción. Hay que ver si es válida
+
+  ; ### Parte 12 - Salida de programa por falta de instrucciones ###
+  cmp rdx, -1                 ; Si la instrucción está cargada de F's, no es válida, entonces salir
+  je _exit
+  ; Hasta este punto, tengo todo filtrado de que sea correcto
+
+  ; ### Parte 13 - PC + 4 ###
+  add r15, 4                  ; PC + 4
+  jmp _predecode
+; END DEBUG!
+
+_predecode:
+  ; ### Parte 14 - Obtener las componentes de la instrucción (opcode, function, ...) ###
+  jmp _exit
+
 _filefound:
-  ; ### Parte X - Mensaje de info - FileFound ###
+  ; ### Parte A - Mensaje de info - FileFound ###
   mov rax,1						             ;Colocar en modo sys_write
   mov rdi,1					               ;Colocar en consola
   mov rsi,const_filefound_txt		 ;Cargar el mensaje
   mov rdx,const_filefound_size	 ;Tamaño del mensaje
   syscall
-  jmp _startPCCounter
+  jmp _readinstructions
 
 _filenotfound:
-  ; ### Parte X - Mensaje de error FILENOTFOUND ###
+  ; ### Parte B - Mensaje de error FILENOTFOUND ###
   mov rax,1						             ;Colocar en modo sys_write
   mov rdi,1					               ;Colocar en consola
   mov rsi,const_filenotfound_txt		 ;Cargar el mensaje
   mov rdx,const_filenotfound_size	 ;Tamaño del mensaje
   syscall
+  jmp _exit
+
+_instoverflow:
+  ; ### Parte C - Mensaje de instrucciones overflow ###
+  mov rax,1						             ;Colocar en modo sys_write
+  mov rdi,1					               ;Colocar en consola
+  mov rsi,instoverflow_txt		 ;Cargar el mensaje
+  mov rdx,instoverflow_size	 ;Tamaño del mensaje
+  syscall
+  jmp _exit
 
 _exit:
   ; ### Cierra el archivo ###
